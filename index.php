f8952a6d71f32edf85d78a341b9bb09dbafc3b18
agratacos <71487758+agratacos@users.noreply.github.com>
2020-09-18 10:51:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="estils.css">
+        <title>IMDB</title>
+    </head>
+    <body>
+        <?php include "Connection.php" ?>
+        <h2>hola desde html</h2>
+        <!-- <img src="
+            <?php
+                
+            ?>
+        
+        
+        " alt="imatge"> -->
+        
+    </body>
+</html>
